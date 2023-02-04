acbdbb4f8dede6681e0b18a9b68692865ac381a1
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-02-04 18:53:39 -0300

    app todo - loading component

--- todo/App.tsx
+++ todo/App.tsx
@@ -1,13 +1,18 @@
 import { useFonts,Inter_400Regular,Inter_700Bold } from "@expo-google-fonts/inter";
 import { StatusBar} from "react-native";
+import { Loading } from "./src/components/Loading";
 import {Home} from "./src/screens/Home";
 
 export default function App() {
-  const [fontsLoaded] =useFonts ({
+  const [fontsLoaded] = useFonts ({
     Inter_400Regular,
     Inter_700Bold,
   });
-  if (fontsLoaded){
+    if (!fontsLoaded){
+    return(
+      <Loading/>
+    )}
+
     return (
       <>
         <StatusBar 
@@ -19,5 +24,4 @@ export default function App() {
       </>
     )
   }
-}
 
