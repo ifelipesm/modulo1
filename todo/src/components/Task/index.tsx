4f20934232ea4e175def6a1657ea3490dfaccb24
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-02-05 18:16:25 -0300

    app todo - Task: updated checkbox + Trash icons

--- todo/src/components/Task/index.tsx
+++ todo/src/components/Task/index.tsx
@@ -1,5 +1,5 @@
 import { View,Text, TouchableOpacity } from "react-native"
-import { Feather } from "@expo/vector-icons"
+import { CheckCircle, Circle, Trash } from 'phosphor-react-native'
 import { styles } from "./styles"
 
 interface taskProps {
@@ -15,17 +15,17 @@ export function Task({description,onRemove,checked=false}:taskProps){
       checked
       ? 
           <>
-            <Feather name="check-circle" color="#4EA8DE" size={24} />
+            <CheckCircle color="#5E60CE" size={24} />
             <Text style={styles.descriptionChecked}>{description}</Text>
           </>
       :
-         <>
-            <Feather name="circle" color="#4EA8DE" size={24} />
+          <>
+            <Circle color="#4EA8DE" size={24} />
             <Text style={styles.description}>{description}</Text>
           </>
       }
       <TouchableOpacity style={styles.removeIconBox} onPress={onRemove}>
-          <Feather name="trash-2" size={20} color="#808080"/>
+          <Trash  size={19} color="#808080"/>
       </TouchableOpacity>
     </View>
   )
