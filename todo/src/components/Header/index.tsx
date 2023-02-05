612e08d090e0fb8a5e048648cd78a5d209d42a1c
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-02-05 01:06:25 -0300

    app todo - header/logo: svg config fix

--- todo/src/components/Header/index.tsx
+++ todo/src/components/Header/index.tsx
@@ -6,7 +6,10 @@ import Logo  from '../../assets/logo.svg';
 
 export function Header(){
   return(
+    <>
     <View style={styles.container}>
+      <Logo style={styles.logo}/>
     </View>
+    </>
   )
 }
--- todo/src/components/Header/styles.ts
+++ todo/src/components/Header/styles.ts
@@ -2,11 +2,16 @@ import { StyleSheet } from "react-native";
 
 
 export const styles = StyleSheet.create({
-container:{
-backgroundColor:'#0D0D0D',
-height: 173,
-},
-logo:{
-height: 32,
-},
+  container:{
+    backgroundColor:'#0D0D0D',
+    height: 173,
+  },
+  logo:{
+    width: 110,
+    height: 32,
+    marginTop: 70,
+    marginLeft:132,
+    marginRight: 132,
+    marginBottom:40,
+  },
 })
