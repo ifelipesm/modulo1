a4d4b4e5aae8579f51f51df014a4f23a4a4d7239
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-02-08 14:58:01 -0300

    IgniteTeams - StyledComponent+Fonts+Loading #2

--- modulo2/igniteteams/App.tsx
+++ modulo2/igniteteams/App.tsx
@@ -1,10 +1,18 @@
 import { Groups } from "@screens/Groups";
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import theme from  './src/theme';
+import { ActivityIndicator } from 'react-native'
+import { Loading } from "@components/Loading";
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({  Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <>
-    <Groups/>
-    </>
+    <ThemeProvider  theme={theme}>
+    { fontsLoaded ? <Groups/> : <Loading/> }
+    </ThemeProvider>
   );
 }
 
