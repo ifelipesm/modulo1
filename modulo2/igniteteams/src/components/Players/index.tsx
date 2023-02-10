5321768f09f60dcdc72e48698144f75100e3ab84
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-02-09 22:22:18 -0300

    IgniteTeams - Players:  PlayerCard + Button #12

--- modulo2/igniteteams/src/components/Players/index.tsx
+++ modulo2/igniteteams/src/components/Players/index.tsx
@@ -1,16 +1,21 @@
-import { ButtonIcon } from '@components/ButtonIcon';
-import { Filter } from '@components/Filter';
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
-import { Input } from '@components/Input';
-import { Container, Form } from './styles';
-import { FlatList } from 'react-native'
 import { useState } from 'react';
+import { FlatList } from 'react-native'
+
+import { Header } from '@components/Header';
+import { Input } from '@components/Input';
+import { Highlight } from '@components/Highlight';
+import { PlayerCard } from '@components/PlayerCard';
+import { Filter } from '@components/Filter';
 import { HeaderList, NumbersOfPlayers } from '@components/Filter/styles';
+
+import { Container, Form } from './styles';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+import { ButtonIcon } from '@components/ButtonIcon';
 
 export function Players() {
   const [team,setTeam] = useState('Time A');
-  const [players, setPlayers] = useState('');
+  const [players, setPlayers] = useState([]);
 
   return (
     <Container>
@@ -49,6 +54,32 @@ export function Players() {
           {players.length}
         </NumbersOfPlayers>
       </HeaderList>
+      
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <PlayerCard 
+            name={item} 
+            onRemove={() => { }}
+          />
+        )}
+        ListEmptyComponent = {()=>  (
+          <ListEmpty
+            message="Não há jogadores neste time."
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          {paddingBottom:100},
+          players.length === 0 && { flex:1 }
+          ]}
+      />
+
+      <Button
+      title="Remover Time"
+      type="SECONDARY"
+      />
     </Container>
   );
 }
