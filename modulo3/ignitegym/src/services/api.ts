67a56f4f03edc76b4c4acb8f58a9ae302965c2e4
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-03-13 20:06:43 -0300

    Feat: Back-End Integration (SignUp) #11

---
+++ modulo3/ignitegym/src/services/api.ts
@@ -0,0 +1,18 @@
+import { AppError } from './../utils/AppError';
+import axios from "axios";
+
+
+ const api = axios.create({
+  baseURL:  'http://192.168.0.102:3333'
+});
+
+api.interceptors.request.use(response => response,error =>{
+  if(error.response && error.response.data){
+  return Promise.reject(new AppError(error.response.data.message));
+  }
+ else{
+  return Promise.reject(new AppError(error));
+}
+});
+
+export {api};
