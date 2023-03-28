348f244c186b02f208cf285c6b0ea98d7b615aa5
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-03-28 17:58:57 -0300

    Feat: OneSignal Push Notifications #2

--- modulo4/igniteshoes/App.tsx
+++ modulo4/igniteshoes/App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,10 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId("your-id")
+
+OneSignal.setEmail('dev@igniteshoes.com');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
