adb4447ec6aeb9b846d7b80a67e0a2a95a62d5d0
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-04-02 18:19:19 -0300

    Feat: SignUp UI #4

--- modulo3/marketplace/src/components/Button.tsx
+++ modulo3/marketplace/src/components/Button.tsx
@@ -2,7 +2,7 @@ import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base'
 
 type Props = IButtonProps & {
   title: string;
-  variant?: 'blue' | 'gray';
+  variant?: 'blue' | 'gray' | 'black';
 }
 
 export function Button({title,variant="blue", ...rest}:Props){
@@ -10,15 +10,15 @@ export function Button({title,variant="blue", ...rest}:Props){
     <ButtonNativeBase
     w="full"
     h={12}
-    bg={variant === "blue" ? "blue.500" : "gray.500" }
+    bg={variant === "blue" ? "blue.500" : ( variant === 'gray' ? "gray.500" : "gray.100" )  }
     borderRadius="lg"
     _pressed={{
-        backgroundColor: variant === "blue" ? "blue.500" : "gray.500" 
+        backgroundColor: variant === "blue" ? "blue.500" : ( variant === 'gray' ? "gray.500" : "gray.100" )
       }}
     {...rest}
     >
       <Text 
-      color={variant === "blue" ? "gray.700" : "gray.200" }
+      color={variant === "blue" ? "gray.700" : (variant === "gray" ? "gray.200" : "gray.700")  }
       fontFamily="heading" 
       fontSize="sm">
         {title}
--- modulo3/marketplace/src/screens/SignUp.tsx
+++ modulo3/marketplace/src/screens/SignUp.tsx
@@ -1,14 +1,74 @@
-import { Center, Text, VStack } from "native-base";
-
+import { Box, Center, Heading, ScrollView, Text, VStack } from "native-base";
+import LogoIcon from '@assets/logo_icon.svg';
+import AvatarCircle from '@assets/avatar.svg';
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+import { AuthFooter } from "@components/AuthFooter";
+import { useNavigation } from "@react-navigation/native";
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 
 export function SignUp(){
+
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  function handleSignIn(){
+    navigation.navigate('signIn');
+  }
+
   return(
-    <VStack flex={1}>
-      <Center>
-        <Text>
-          SignUp
-        </Text>
-      </Center>
-    </VStack>
+    <ScrollView 
+    showsVerticalScrollIndicator={false}
+    contentContainerStyle={{flexGrow: 1}}>
+      
+      <VStack flex={1} bgColor="gray.600">
+        <Box mx={12}>
+          <Box mt={16} mb={8} >
+            <Center>
+              <Box mb={3}>
+                <LogoIcon/>
+              </Box>
+              <Heading color="gray.100" fontSize="lg" fontFamily="heading" mb={2} >
+                Boas vindas!
+              </Heading>
+              <Text color="gray.200" textAlign="center" fontSize="sm" fontFamily="body" mb={4} >
+                Crie sua conta e use o espaço para comprar itens variados e vender seus produtos
+              </Text>
+            </Center>
+          </Box>
+            <Center mb={4} >
+              <AvatarCircle/>
+            </Center>
+            <Center>
+              <Input
+                placeholder="Nome"
+              />
+              <Input
+                placeholder="E-mail"
+                autoCapitalize="none"
+              />
+              <Input
+                placeholder="Telefone"
+                keyboardType="number-pad"
+              />
+              <Input
+                placeholder="Senha"
+                autoCapitalize="none"
+                secureTextEntry
+              />
+              <Input
+                placeholder="Confirmar senha"
+                autoCapitalize="none"
+                secureTextEntry
+              />
+            </Center>
+            <Center mt={6} mb={12} >
+              <Button title="Criar" variant="black" />
+            </Center>
+            <Box>
+              <AuthFooter action={handleSignIn} label="Já tem uma conta?" title="Ir para o login" />
+            </Box>
+        </Box>
+      </VStack>
+    </ScrollView>
   )
 }
